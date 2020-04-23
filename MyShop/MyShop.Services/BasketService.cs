063d65187580e2d551aeaba306a38d4473413b0a
Shyam Singh <singhshyam473@gmail.com>
2020-04-23 20:43:01 +0530

    Basket service-2

--- MyShop/MyShop.Services/BasketService.cs
+++ MyShop/MyShop.Services/BasketService.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Web;
 using MyShop.Core.Contracts;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 
 namespace MyShop.Services
 {
@@ -103,5 +104,56 @@ namespace MyShop.Services
                 basketContext.Commit();
             }
         }
+
+        public List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext)
+        {
+            Basket basket = GetBasket(httpContext, false);
+
+            if (basket != null)
+            {
+                var results = (from b in basket.BasketItems
+                        join p in productContext.Collection() on b.ProductId equals p.Id
+                        select new BasketItemViewModel()
+                        {
+                            Id = b.Id,
+                            Quantity = b.Quanity,
+                            ProductName = p.Name,
+                            Image = p.Image,
+                            Price = p.Price
+                        }
+                    ).ToList();
+                return results;
+            }
+            else
+            {
+                return new List<BasketItemViewModel>();
+            }
+        }
+
+
+        public BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext)
+        {
+            Basket basket = GetBasket(httpContext, false);
+            BasketSummaryViewModel model = new BasketSummaryViewModel(0, 0);
+            if (basket != null)
+            {
+                int? basketCount = (from item in basket.BasketItems
+                    select item.Quanity).Sum();
+
+                decimal? basketTotal = (from item in basket.BasketItems
+                    join p in productContext.Collection() on item.ProductId equals p.Id
+                    select item.Quanity * p.Price).Sum();
+
+                model.BasketCount = basketCount ?? 0;
+                model.BasketTotal = basketTotal ?? decimal.Zero;
+
+                return model;
+            }
+            else
+            {
+                return model;
+            }
+        }
+
     }
 }
