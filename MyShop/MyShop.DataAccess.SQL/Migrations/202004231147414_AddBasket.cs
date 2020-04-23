ec61fe657a3afcb25f0b9c51f66a834106573443
Shyam Singh <singhshyam473@gmail.com>
2020-04-23 19:25:02 +0530

    basket

---
+++ MyShop/MyShop.DataAccess.SQL/Migrations/202004231147414_AddBasket.cs
@@ -0,0 +1,29 @@
+﻿namespace MyShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBasket : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BasketItems",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        BasketId = c.String(),
+                        ProductId = c.String(),
+                        Quanity = c.Int(nullable: false),
+                        CreateAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.BasketItems");
+        }
+    }
+}
---
+++ MyShop/MyShop.DataAccess.SQL/Migrations/202004231150497_AddBasket1.cs
@@ -0,0 +1,32 @@
+﻿namespace MyShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBasket1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Baskets",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        CreateAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AlterColumn("dbo.BasketItems", "BasketId", c => c.String(maxLength: 128));
+            CreateIndex("dbo.BasketItems", "BasketId");
+            AddForeignKey("dbo.BasketItems", "BasketId", "dbo.Baskets", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.BasketItems", "BasketId", "dbo.Baskets");
+            DropIndex("dbo.BasketItems", new[] { "BasketId" });
+            AlterColumn("dbo.BasketItems", "BasketId", c => c.String());
+            DropTable("dbo.Baskets");
+        }
+    }
+}
